bd8b458c2d5a3c9f9398cd89f4157aa0fb40a219
sudoman11 <r.deepanrajofficial@gmail.com>
2025-06-23 00:43:10 +0530

    updated files

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+// filepath: c:\Users\Innocent\Documents\company website\company-website\project\postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
